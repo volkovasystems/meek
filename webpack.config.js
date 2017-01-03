3c9b920795b96777a830c78692bcfa5522fba189
johnlenon <johnlenonmaghanoy@gmail.com>
2017-01-03 14:51:11 +0800

    Improve build procedure.

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const webpack = require( "webpack" );
+const ResolverPlugin = webpack.ResolverPlugin;
+const DirectoryDescriptionFilePlugin = ResolverPlugin.DirectoryDescriptionFilePlugin;
+const UglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
+
+module.exports = {
+	"entry": "./meek.support.js",
+	"resolve": {
+		"modulesDirectories": [ "bower_components", "node_modules" ]
+	},
+	"module": {
+		"preLoaders": [
+			{
+				"test": /\.support\.js$/,
+				"loader": "source-map-loader"
+			}
+		]
+	},
+	"output": {
+		"library": "meek",
+		"libraryTarget": "umd",
+		"filename": "meek.deploy.js"
+	},
+	"plugins": [
+		new ResolverPlugin( new DirectoryDescriptionFilePlugin( "bower.json", [ "support" ] ) ),
+		new ResolverPlugin( new DirectoryDescriptionFilePlugin( ".bower.json", [ "main" ] ) ),
+		new UglifyJsPlugin( {
+			"compress": { "warnings": false },
+			"comments": false,
+			"sourceMap": true,
+			"mangle": false
+		} )
+	],
+	"devtool": "#inline-source-map"
+};
