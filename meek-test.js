6837c56b41dd896eb4d1e236ae4c67ff201f2fe3
johnlenon <johnlenonmaghanoy@gmail.com>
2017-06-28 16:56:46 +0800

    Updated server/client test.

--- meek-test.js
+++ meek-test.js
@@ -1,3 +1,17 @@
+
+const assert = require( "assert" );
 const meek = require( "./meek.js" );
 
-console.log( meek( ) )
+assert.deepEqual( meek( SUCCESS, { "name": "simple" } ), { "status": "success", "data": { "name": "simple" } },
+					"should be deeply equal" );
+
+assert.deepEqual( meek( PROMPT, { "name": "simple" } ), { "status": "prompt", "data": { "name": "simple" } },
+					"should be deeply equal" );
+
+assert.deepEqual( meek( ISSUE, { "name": "simple" } ), { "status": "issue", "data": { "name": "simple" } },
+					"should be deeply equal" );
+
+assert.deepEqual( meek( FATAL, { "name": "simple" } ), { "status": "fatal", "data": { "name": "simple" } },
+					"should be deeply equal" );
+
+console.log( "ok" );
